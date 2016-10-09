36f1e60ffeb255b7c6fd4822bc82f6d0ed888e2c
Saiyed <smmuzzammil@gmail.com>
2016-10-09 21:48:01 +0500

    Add three fiekls in search bar donor(Advanced search ,Near By,Recent), background colour is not final.

--- Blood Donor/Blood Donor/HomeView.swift
+++ Blood Donor/Blood Donor/HomeView.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class HomeView: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate  , UICollectionViewDelegateFlowLayout{
+class HomeView: UIViewController{
     @IBOutlet weak var homeMenu: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,52 +29,52 @@ class HomeView: UIViewController, UICollectionViewDataSource, UICollectionViewDe
     }
     
     
-    let reuseIdentifier = "cell"
-    var items = ["Become Donor ", "Request Donor" , "Search Donor"]
-    
-        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.items.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-
-        collectionView.backgroundColor = UIColor.white
-        let myLabel = (cell.viewWithTag(100)! as! UILabel)
-        myLabel.text = self.items[(indexPath as NSIndexPath).item]
-        cell.backgroundColor = UIColor.black
-        cell.backgroundColor = UIColor.black
-
-        return cell
-    }
-    
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        
-        var viewControllerObj : UIViewController
-        
-        switch((indexPath as NSIndexPath).item){
-            case 0:
-                viewControllerObj = (self.storyboard?.instantiateViewController(withIdentifier: "BecomeDonorVC") as? BecomeDonorVC)!
-            case 1:
-                viewControllerObj = (self.storyboard?.instantiateViewController(withIdentifier: "RequestDonorVC") as? RequestDonorVC)!
-            case 2:
-             viewControllerObj = (self.storyboard?.instantiateViewController(withIdentifier: "SearchDonorVC") as? SearchDonorVC)!
-            default:
-                return
-        }
-       
-        self.navigationController?.pushViewController(viewControllerObj, animated: true)
-
-        // handle tap events
-        print("You selected cell #\((indexPath as NSIndexPath).item)!")
-    }
-    
-    func collectionView(_ collectionView: UICollectionView,
-        layout collectionViewLayout: UICollectionViewLayout,
-        sizeForItemAt indexPath: IndexPath) -> CGSize {
-            return CGSize(width: 100, height: 70)
-    }
+//    let reuseIdentifier = "cell"
+//    var items = ["Become Donor ", "Request Donor" , "Search Donor"]
+//    
+//        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return self.items.count
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+//
+//        collectionView.backgroundColor = UIColor.white
+//        let myLabel = (cell.viewWithTag(100)! as! UILabel)
+//        myLabel.text = self.items[(indexPath as NSIndexPath).item]
+//        cell.backgroundColor = UIColor.black
+//        cell.backgroundColor = UIColor.black
+//
+//        return cell
+//    }
+//    
+//    
+//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        
+//        var viewControllerObj : UIViewController
+//        
+//        switch((indexPath as NSIndexPath).item){
+//            case 0:
+//                viewControllerObj = (self.storyboard?.instantiateViewController(withIdentifier: "BecomeDonorVC") as? BecomeDonorVC)!
+//            case 1:
+//                viewControllerObj = (self.storyboard?.instantiateViewController(withIdentifier: "RequestDonorVC") as? RequestDonorVC)!
+//            case 2:
+//             viewControllerObj = (self.storyboard?.instantiateViewController(withIdentifier: "SearchDonorVC") as? SearchDonorVC)!
+//            default:
+//                return
+//        }
+//       
+//        self.navigationController?.pushViewController(viewControllerObj, animated: true)
+//
+//        // handle tap events
+//        print("You selected cell #\((indexPath as NSIndexPath).item)!")
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView,
+//        layout collectionViewLayout: UICollectionViewLayout,
+//        sizeForItemAt indexPath: IndexPath) -> CGSize {
+//            return CGSize(width: 100, height: 70)
+//    }
     
 }   
